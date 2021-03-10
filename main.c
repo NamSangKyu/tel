bffa909651e16bc842100afdc0e0a854f82ea431
nam2626 <nam2626@gmail.com>
2021-03-10 16:18:56 +0900

    최초기본 소스 코드 작성:

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+enum{INSERT, DELETE,SEARCH,UPDATE,EXIT};
+typedef struct tel{
+	char name[30];
+	char phone[20];
+}Tel;
+
+Tel arr[10];
+int idx=0;
+
+void PrintMenu(){
+
+}
+void InsertTel(){
+
+}
+void DeleteTel(){
+
+}
+void SearchTel(){
+
+}
+void UpdateTel(){
+
+}
+int main(void)
+{
+	
+	return 0;
+}
+
