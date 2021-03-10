3a5bda30d2f9d126b9a49e035dbf3ce44703311b
nam2626 <nam2626@gmail.com>
2021-03-10 16:39:32 +0900

    전화번호부 작업 완료

--- main.c
+++ main.c
@@ -29,7 +29,19 @@ void InsertTel(){
 	idx++;
 }
 void DeleteTel(){
-
+	char name[30];
+	int i,j;
+	printf("연락처 삭제를 시작합니다.\n");
+	printf("삭제할 이름을 입력하세요 : ");  scanf("%s",name);
+	for(i=0;i<idx;i++){
+		if(strcmp(arr[i].name,name)==0){
+			for(j=i;j<idx-1;j++)
+				arr[j] = arr[j+1];
+			idx--;
+			return;
+		}
+	}
+	printf("삭제할 데이터가 없습니다.\n");
 }
 void SearchTel(){
 	char name[30];
@@ -45,7 +57,19 @@ void SearchTel(){
 	printf("검색 결과가 없습니다.\n");
 }
 void UpdateTel(){
-
+	char name[30];
+	int i;
+	printf("수정할 연락처 검색을 시작합니다.\n");
+	printf("수정할 대상의 이름을 입력하세요 : ");  scanf("%s",name);
+	for(i=0;i<idx;i++){
+		if(strcmp(arr[i].name,name)==0){
+			printf("수정할 이름 : "); scanf("%s",arr[i].name);
+			printf("수정할 연락처 : "); scanf("%s",arr[i].phone);
+			printf("수정 완료\n");
+			return;
+		}
+	}
+	printf("수정할 데이터가 없습니다.\n");
 }
 int main(void)
 {
