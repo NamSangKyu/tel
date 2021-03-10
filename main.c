c9e493058ccdee9246ed21f9b46defbfc8bdfa2c
nam2626 <nam2626@gmail.com>
2021-03-10 16:24:57 +0900

    메뉴 제어까지 완료

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-enum{INSERT, DELETE,SEARCH,UPDATE,EXIT};
+enum{INSERT=1, DELETE,SEARCH,UPDATE};
 typedef struct tel{
 	char name[30];
 	char phone[20];
@@ -10,10 +10,16 @@ Tel arr[10];
 int idx=0;
 
 void PrintMenu(){
-
+	printf("****MENU****\n");
+	printf("1. Insert\n");
+	printf("2. Delete\n");
+	printf("3. Search\n");
+	printf("4. Update\n");
+	printf("0. EXIT\n");
+	
 }
 void InsertTel(){
-
+	
 }
 void DeleteTel(){
 
@@ -26,7 +32,26 @@ void UpdateTel(){
 }
 int main(void)
 {
-	
+	int no = -1;
+	while(no != 0){
+		PrintMenu();
+		printf("input menu no : ");
+		scanf("%d",&no);
+		switch(no){
+		case INSERT:
+			InsertTel();
+			break;
+		case UPDATE:
+			UpdateTel();
+			break;
+		case DELETE:
+			DeleteTel();
+			break;
+		case SEARCH:
+			SearchTel();
+			break;
+		}
+	}
 	return 0;
 }
 
