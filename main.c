f2761b4a473c3caefd4cb32ff92bca5423ea6736
nam2626 <nam2626@gmail.com>
2021-03-10 16:32:42 +0900

    삽입, 검색 완료

--- main.c
+++ main.c
@@ -19,13 +19,30 @@ void PrintMenu(){
 	
 }
 void InsertTel(){
-	
+	printf("연락처 등록을 시작합니다.\n");
+	if(idx == 10){
+		printf("더 이상 저장할 공간이 없습니다\n");
+		return;
+	}
+	printf("이름 : "); scanf("%s",arr[idx].name);
+	printf("연락처 : "); scanf("%s",arr[idx].phone);
+	idx++;
 }
 void DeleteTel(){
 
 }
 void SearchTel(){
-
+	char name[30];
+	int i;
+	printf("연락처 검색을 시작합니다.\n");
+	printf("이름을 입력하세요 : ");  scanf("%s",name);
+	for(i=0;i<idx;i++){
+		if(strcmp(arr[i].name,name)==0){
+			printf("이름 : %s, 연락처 : %s\n",arr[i].name, arr[i].phone);
+			return;
+		}
+	}
+	printf("검색 결과가 없습니다.\n");
 }
 void UpdateTel(){
 
